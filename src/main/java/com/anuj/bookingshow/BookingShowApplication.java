6743a155d75870ecdf960fc4463720d88c6809d3
Anuj <anujcse525@gmail.com>
2021-02-01 17:28:48 +0530

    Initial commit

---
+++ src/main/java/com/anuj/bookingshow/BookingShowApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.bookingshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuj/bookingshow/BookingShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.bookingshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
